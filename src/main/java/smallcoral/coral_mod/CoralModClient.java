2888fa4bf2d249d535e3316ba5e5bbf10534efbc
SmallCoral <Shan_Hu_MC@outlook.com>
2023-07-07 22:14:04 +0800

    Initial commit

---
+++ src/main/java/smallcoral/coral_mod/CoralModClient.java
@@ -0,0 +1,10 @@
+package smallcoral.coral_mod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CoralModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/smallcoral/coral_mod/CoralModDataGenerator.java
@@ -0,0 +1,11 @@
+package smallcoral.coral_mod;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class CoralModDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
